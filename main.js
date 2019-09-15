79c7227113e9a8e03d68064484b01692ec1f9068
Daria <daryalutkova@yandex.ru>
2019-09-15 16:22:42 +0300

    SPA

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.onload = () =>{
 	let ul = document.getElementById('list1');
 	let btn2  = document.getElementById('send');
 	let btn3 = document.getElementById('delete');
-	btn.onclick = () =>{
+	function loadAuthor(){
 		ul.innerHTML = '';
 		fetch('http://localhost:3000/posts')
 			.then(result => result.json())
@@ -12,17 +12,20 @@ window.onload = () =>{
 				data.forEach((item) =>{
 					let li = document.createElement('li');
 					li.classList.add("list-group-item");
-					li.innerHTML = `<b>${item.author}</b> : ${item.title}`;
+					li.innerHTML = `<p><b>${item.author}</b> : ${item.title}<button type="button" class="close" aria-label="Close"><span aria-hidden="true" id="btn_close${item.id}" data-id="${item.id}">&times;</span></button></p>`;
 					ul.appendChild(li);
 				})
 			})
-	};
-	/*let request = {
-		"method" : 'POST',
-		"body" : JSON.stringify({"title": 'love', "author": 'lovers'}),
-		'headers' : {'Content-Type': 'application/json'}
-	}*/
-	btn2.onclick = (event) =>{
+	}
+	function deleteAuthor(id){
+		fetch('http://localhost:3000/posts/'+id, {
+			"method" : "DELETE"
+		})
+			.then(console.log("Author "+ id + " is deleted"))
+			.catch(er => console.log(er));
+			loadAuthor();
+	}
+	function addAuthor(){
 		event.preventDefault();
 		let author = document.getElementById('author').value;
 		let title = document.getElementById('title').value;
@@ -32,13 +35,32 @@ window.onload = () =>{
 			"headers" : {"Content-Type" : "application/json"}
 		})
 			.then(console.log('saved'))
-			.catch(error => console.log(error))
+			.catch(error => console.log(error));
+		loadAuthor();
+	}
+	btn.onclick = () =>{
+		loadAuthor();
 	};
+	btn2.onclick = (event) =>{
+		addAuthor();
+	};
+	ul.addEventListener("click", (event) =>{
+		let target = event.target;
+		if (target.tagName != "SPAN") return;
+		deleteAuthor(target.dataset.id);
+		})
+
+	/*let request = {
+		"method" : 'POST',
+		"body" : JSON.stringify({"title": 'love', "author": 'lovers'}),
+		'headers' : {'Content-Type': 'application/json'}
+	}*/
+	/*
 	btn3.onclick = () =>{
 		fetch('http://localhost:3000/posts/5', {
 			"method" : "DELETE",
 		})
 			.then(console.log('deleted'))
 			.catch(er => console.log(er))
-	}
+	}*/
 }
